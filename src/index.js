00ecea4eb07a9d33ff40de14aa53c80104b25de6
dekelohad <dekelohad@gmail.com>
2020-10-18 17:46:19 +0300

    Added CommitMessage Component

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 import './index.css';
+
 const FileList = ({ files }) => (
   <table className="file-list">
   <tbody>
@@ -17,40 +18,51 @@ const FileList = ({ files }) => (
 
   const FileListItem = ({ file }) => (
     <tr className="file-list-item">
-      <FileName file={file}/>
+      <FileName file={file} />
+      <CommitMessage
+      commit={file.latestCommit} />
     </tr>
     );
     FileListItem.propTypes = {
     file: PropTypes.object.isRequired
     };
-    
-const FileIcon = ({ file }) =>{
-    let icon = 'fa-file-text-o';
-        if(file.type === 'folder') {
-        icon = 'fa-folder';
+
+  const FileName =  ({ file }) => {
+    return (
+      <>
+      <FileIcon file={file}/>
+      <td className="file-name">{file.name}</td>
+      </>
+    );
       }
-      return (
-          <td className="file-icon">
-          <i className={`fa ${icon}`}/>
-          </td>
-      );
-    }
-    FileIcon.propTypes = {
+    FileName.propTypes = {
     file: PropTypes.object.isRequired
     };
 
-
-    const FileName =  ({ file }) => {
-      return (
-        <>
-        <FileIcon file={file}/>
-        <td className="file-name">{file.name}</td>
-        </>
+  const FileIcon = ({ file }) =>{
+      let icon = 'fa-file-text-o';
+          if(file.type === 'folder') {
+          icon = 'fa-folder';
+        }
+        return (
+            <td className="file-icon">
+            <i className={`fa ${icon}`}/>
+            </td>
         );
       }
-      FileName.propTypes = {
+      FileIcon.propTypes = {
       file: PropTypes.object.isRequired
       };
+
+  const CommitMessage = ({ commit }) => (
+    <td className="commit-message">
+      {commit.message}
+    </td>
+    );
+    CommitMessage.propTypes = {
+    commit: PropTypes.object.isRequired
+    };
+
 
 const testFiles = [
   {
